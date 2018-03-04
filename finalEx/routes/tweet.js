a0a2e41c7e59cae0ad22f9e72359e7c09229bdae
rama <ramakrishna8051@gmail.com>
2018-03-04 12:27:24 -0800

    real exercise

--- finalEx/routes/tweet.js
+++ finalEx/routes/tweet.js
@@ -10,101 +10,44 @@ router.get('/', function (req, res, next) {
 
 router.get('/tweets', function (req, res, next) {
     //  return tweets
-    Tweet.find({})
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
 
-    //  return recent tweets based on postedDate (sort) 
-    /*Tweet.find({}).sort('-postedDate')
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })*/
+    //res.send(data);
+
+    //  if there is time    // return recent tweets based on postedDate (sort) 
+
 });
 
 router.get('/tweetContent/:tweetContent', function (req, res, next) {
     let tweetContent = req.params.tweetContent
     //  return tweets based on tweet conetent like
-    Tweet.find({ tweetContent: new RegExp(tweetContent, 'i') })
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    /*Tweet.find({ tweetContent: { $regex: tweetContent, $options: 'i' } })
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
 
-    Tweet.find({ tweetContent: { $regex: tweetContent, $options: 'i' } }).where('tweetUser').equals('hello').select({ tweetUser: 1, tweetContent: 1, _id: 1 })
-        .then((data) => {
-            console.log(data)
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })*/
+    //res.send(data);
 });
 
 router.get('/tweetUser/:tweetUser', function (req, res, next) {
     let tweetUser = req.params.tweetUser
     //  return tweets posted by a tweetUser
-    Tweet.find({ tweetUser: tweetUser })
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+
+    //res.send(data);
 });
 
 router.post('/addTweet', function (req, res, next) {
     //  insert a tweet 
-    let tweet = new Tweet({ tweetUser: req.body.tweetUser, tweetContent: req.body.tweetContent });
-    //console.log(tweet);
-    tweet.save()
-        .then((e) => {
-            //console.log(e);
-            res.send(JSON.stringify(tweet));
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+
+    //res.send(JSON.stringify(tweet));
 });
 
 router.post('/updateTweet', function (req, res, next) {
     //  update the tweet content based on _id
-    Tweet.update({ _id: req.body.tweetId }, { $set: { tweetContent: req.body.tweetContent } })
-        .then((e) => {
-            //console.log(e);
-            res.send(JSON.stringify({ check: true }));
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+
+    //res.send(JSON.stringify({ check: true }));
 });
 
 router.post('/deleteTweet', function (req, res, next) {
     let tweetId = req.body.tweetId
     //  remove a tweet based on _id
-    Tweet.remove({ _id: tweetId })
-        .then((e) => {
-            //console.log(e);
-            res.send(JSON.stringify({ check: true }));
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+
+    //res.send(JSON.stringify({ check: true }));
 });
 
 module.exports = router;
--- finalEx/schema/tweet.js
+++ finalEx/schema/tweet.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 //  create a Tweet Schema and return a Tweet model
-const TweetSchema = new schema({
-    tweetUser: String,
-    tweetContent: String,
-    postedDate: { type: Date, default: Date.now }
-});
 
-const Tweet = mongoose.model('tweets', TweetSchema);
+
+const Tweet = '';
 
 module.exports = Tweet;

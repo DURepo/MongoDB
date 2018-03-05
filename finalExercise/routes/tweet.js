13091632841a3171f8c98e337f99f30a5f26be58
Ujjval <ujjvalkumaria@gmail.com>
2018-03-04 16:02:17 -0800

    fixed tests

--- finalExercise/routes/tweet.js
+++ finalExercise/routes/tweet.js
@@ -9,11 +9,11 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/tweets', function (req, res, next) {
-    //  return tweets
+    // return tweets
 
-    //res.send(data);
+    // send data back to the view
 
-    //  if there is time    // return recent tweets based on postedDate (sort) 
+    // OPTIONAL TASK: return recent tweets based on postedDate (sort)
 
 });
 
@@ -21,33 +21,33 @@ router.get('/tweetContent/:tweetContent', function (req, res, next) {
     let tweetContent = req.params.tweetContent
     //  return tweets based on tweet conetent like
 
-    //res.send(data);
+    // send data back to the view
 });
 
 router.get('/tweetUser/:tweetUser', function (req, res, next) {
     let tweetUser = req.params.tweetUser
     //  return tweets posted by a tweetUser
 
-    //res.send(data);
+    // send data back to the view
 });
 
 router.post('/addTweet', function (req, res, next) {
     //  insert a tweet 
 
-    //res.send(JSON.stringify(tweet));
+    // send the data to the view as a Json string, example: res.send(JSON.stringify(tweet));
 });
 
 router.post('/updateTweet', function (req, res, next) {
-    //  update the tweet content based on _id
+    // update the tweet content based on _id
 
-    //res.send(JSON.stringify({ check: true }));
+    // send a JS object with check property set to true as a Json string like in the previous example
 });
 
 router.post('/deleteTweet', function (req, res, next) {
     let tweetId = req.body.tweetId
     //  remove a tweet based on _id
 
-    //res.send(JSON.stringify({ check: true }));
+    // send a JS object with check property set to true as a Json string like in the previous example
 });
 
 module.exports = router;
